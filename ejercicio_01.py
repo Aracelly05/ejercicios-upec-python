d74858d5e427ccf344910d27e08fae7095aa657f
Aracelly05 <aracelly.salazar@upec.edu.ec>
2026-05-27 23:38:24 +0000

    ejercicio_01

---
+++ ejercicio_01.py
@@ -0,0 +1,14 @@
+# Mi primer programa en Python
+# Programa que calcula la suma de 3 numeros y su promedio 
+numero_1 = 20
+numero_2 = 30
+numero_3 = 40
+suma = numero_1 + numero_2 + numero_3
+print("La suma de los tres numeros es: ", suma)
+promedio = suma/3
+print ("El promedio de los tres numeros es: ", promedio)
+# Verificar si numero_1 es par o impar
+if numero_1 % 2 == 0:
+    print("El numero_1 es par")
+else:
+    print("El numero_1 es impar")
